512d86247a3bcbc53270c26e020d83ef92104084
John Cook <coder-a599-7927@johncook.co.uk>
2021-06-14 19:40:20 +0100

    Split custom EditableCMD commands into separate repository

---
+++ Trees/Trees.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.Versioning;
+using System.Text.RegularExpressions;
+using uk.JohnCook.dotnet.EditableCMDLibrary.Commands;
+using uk.JohnCook.dotnet.EditableCMDLibrary.ConsoleSessions;
+using uk.JohnCook.dotnet.EditableCMDLibrary.Interop;
+
+namespace uk.JohnCook.dotnet.EditableCMD.Plugins
+{
+    /// <summary>
+    /// TREES command
+    /// </summary>
+    [SupportedOSPlatform("windows")]
+    public class Trees : ICommandInput
+    {
+        #region Plugin Implementation Details
+        /// <summary>
+        /// Name of the plugin.
+        /// </summary>
+        public string Name => "Trees";
+        /// <summary>
+        /// Summary of the plugin's functionality.
+        /// </summary>
+        public string Description => "Handles TREES command - prints some ASCII art trees.";
+        /// <summary>
+        /// Author's name (can be <see cref="string.Empty"/>)
+        /// </summary>
+        public string AuthorName => "John Cook";
+        /// <summary>
+        /// Author's Twitch username (can be <see cref="string.Empty"/>)
+        /// </summary>
+        public string AuthorTwitchUsername => "WatfordJC";
+        /// <summary>
+        /// An array of the keys handled by the plugin. For commands, this should be <see cref="ConsoleKey.Enter"/>.
+        /// </summary>
+        public ConsoleKey[] KeysHandled => new ConsoleKey[] { ConsoleKey.Enter };
+        /// <summary>
+        /// Whether the plugin handles keys/commands input in normal mode (such as a command entered at the prompt).
+        /// </summary>
+        public bool NormalModeHandled => true;
+        /// <summary>
+        /// Whether the plugin handles keys input in edit mode.
+        /// </summary>
+        public bool EditModeHandled => false;
+        /// <summary>
+        /// Whether the plugin handles keys input in mark mode.
+        /// </summary>
+        public bool MarkModeHandled => false;
+        /// <summary>
+        /// An array of commands handled by the plugin, in lowercase.
+        /// </summary>
+        public string[] CommandsHandled => new string[] { "trees" };
+        #endregion
+
+        private string regexCommandString = string.Empty;
+
+        /// <summary>
+        /// Called when adding an implementation of the interface to the list of event handlers. Approximately equivalent to a constructor.
+        /// </summary>
+        /// <param name="state">The <see cref="ConsoleState"/> for the current console session.</param>
+        public void Init(ConsoleState state)
+        {
+            // Add all commands listed in CommandsHandled to the regex string for matching if this plugin handles the command.
+            if (KeysHandled.Contains(ConsoleKey.Enter) && CommandsHandled.Length > 0)
+            {
+                regexCommandString = string.Concat("^(", string.Join('|', CommandsHandled), ")( .*)?$");
+            }
+        }
+
+        /// <summary>
+        /// Event handler for the TREES command
+        /// </summary>
+        /// <param name="sender">Sender of the event</param>
+        /// <param name="e">The ConsoleKeyEventArgs for the event</param>
+        public void ProcessCommand(object sender, NativeMethods.ConsoleKeyEventArgs e)
+        {
+            // Return early if we're not interested in the event
+            if (e.Handled || // Event has already been handled
+                !e.Key.KeyDown || // A key was not pressed
+                !KeysHandled.Contains(e.Key.ConsoleKey) || // The key pressed wasn't one we handle
+                e.State.EditMode // Edit mode is enabled
+                )
+            {
+                return;
+            }
+            // If current input matches TREES, we are handling the event
+            else if (!string.IsNullOrEmpty(regexCommandString) && Regex.Match(e.State.Input.Text.ToString().Trim(), regexCommandString, RegexOptions.IgnoreCase | RegexOptions.CultureInvariant).Success)
+            {
+                e.Handled = true;
+            }
+            // In all other cases we are not handling the event
+            else
+            {
+                return;
+            }
+
+            ConsoleColor foreground = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(string.Concat("\n\n", strings.treeAsciiArt, "\n"));
+            Console.ForegroundColor = foreground;
+            ConsoleOutput.WritePrompt(e.State, false);
+        }
+    }
+}
